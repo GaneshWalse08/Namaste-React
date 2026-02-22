b552d831bbd812575cdda8f83eba330d1710361b
Ganesh Walse <ganeshwalse1975@gmail.com>
2026-02-23 04:09:43 +0530

    next

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello Ganesh!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="title">Hello! I am Ganesh.</h1>
+);
+
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <h2>Welcome Sir.</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+root.render(<HeadingComponent />);
